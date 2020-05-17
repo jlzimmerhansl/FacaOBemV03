abe87ebf0ea594036d81bfe615143c4c62b85008
Jessica Zimmerhansl <jlzimmerhansl@gmail.com>
2020-05-17 06:58:11 -0700

    Insercao de nova atividade e redirecionamento do botão de iniciar

--- app/src/main/java/com/example/facaobemv03/MainActivity.java
+++ app/src/main/java/com/example/facaobemv03/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.facaobemv03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void iniciarPaginaDoador(View view){
+        Intent intent = new Intent(this, Doador.class);
+        startActivity(intent);
+    }
+
 }
