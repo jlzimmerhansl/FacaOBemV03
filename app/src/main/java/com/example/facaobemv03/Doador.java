d16d5f6451d8cd8d0dea8e24ecf9a8166cc69012
Jessica Zimmerhansl <jlzimmerhansl@gmail.com>
2020-06-14 16:17:26 -0700

    :nail_care: Ajuste de menus

--- app/src/main/java/com/example/facaobemv03/Doador.java
+++ app/src/main/java/com/example/facaobemv03/Doador.java
@@ -2,15 +2,11 @@ package com.example.facaobemv03;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.MenuItem;
-import android.view.View;
 
 public class Doador extends AppCompatActivity {
 
@@ -21,14 +17,6 @@ public class Doador extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -36,7 +24,7 @@ public class Doador extends AppCompatActivity {
         int id = item.getItemId();
 
         switch (id){
-            case R.id.action_menu_ok:
+            case R.id.action_guardar_doador:
                 break;
             case R.id.action_menu_cancelar:
                 break;
