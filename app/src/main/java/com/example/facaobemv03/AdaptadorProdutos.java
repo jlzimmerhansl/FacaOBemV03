a2b19c681dd8e1d4be392c2c3ff58f0d9a25fd56
Jessica Zimmerhansl <jlzimmerhansl@gmail.com>
2020-06-25 15:42:35 -0700

    feat: Criacao do adaptador para a classe Produtos

---
+++ app/src/main/java/com/example/facaobemv03/AdaptadorProdutos.java
@@ -0,0 +1,79 @@
+package com.example.facaobemv03;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.facaobemv03.Models.DoadorModelo;
+import com.example.facaobemv03.Models.ProdutoModelo;
+
+import org.w3c.dom.Text;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdaptadorProdutos extends RecyclerView.Adapter<AdaptadorProdutos.ViewHolderProdutos> {
+
+    private final Context context;
+    private Cursor cursor = null;
+
+    public AdaptadorProdutos(Context context) {
+        this.context = context;
+    }
+
+    public void setCursor(Cursor cursor){
+        if(cursor != this.cursor){
+            this.cursor = cursor;
+            notifyDataSetChanged();
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolderProdutos onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemProdutos = LayoutInflater.from(context).inflate(R.layout.item_produto, parent, false);
+        return new ViewHolderProdutos(itemProdutos);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderProdutos holder, int position) {
+        cursor.moveToPosition(position);
+        ProdutoModelo produtoModelo = Converte.cursorToProduto(cursor);
+        holder.setProduto(produtoModelo);
+    }
+
+
+
+
+    @Override
+    public int getItemCount() {
+        if(cursor == null) return 0;
+
+        return cursor.getCount();
+    }
+
+    public class ViewHolderProdutos extends RecyclerView.ViewHolder{
+        private final TextView textViewNomeProduto;
+        private final TextView textViewQuantidade;
+        private ProdutoModelo produtoModelo = null;
+
+
+        public ViewHolderProdutos(@NonNull View itemView) {
+            super(itemView);
+
+            textViewNomeProduto = (TextView) itemView.findViewById(R.id.textViewNomeProduto);
+            textViewQuantidade = (TextView) itemView.findViewById(R.id.textViewQuantidade);
+        }
+
+        public void setProduto(ProdutoModelo produtoModelo){
+            this.produtoModelo = produtoModelo;
+
+            textViewNomeProduto.setText(produtoModelo.getNomeProduto());
+            textViewQuantidade.setText(String.valueOf(produtoModelo.getQuantidade()));
+        }
+    }
+}
--- app/src/main/java/com/example/facaobemv03/LIstaProdutosFragment.java
+++ app/src/main/java/com/example/facaobemv03/LIstaProdutosFragment.java
@@ -1,58 +1,30 @@
 package com.example.facaobemv03;
 
+import android.content.Context;
+import android.content.CursorLoader;
+import android.database.Cursor;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CursorAdapter;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LIstaProdutosFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class LIstaProdutosFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class LIstaProdutosFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    public LIstaProdutosFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LIstaProdutosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LIstaProdutosFragment newInstance(String param1, String param2) {
-        LIstaProdutosFragment fragment = new LIstaProdutosFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -60,5 +32,32 @@ public class LIstaProdutosFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_l_ista_produtos, container, false);
+    }
+
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return null;
+    }
+
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    /**
+     * Called when a previously created loader is being reset, and thus
+     * making its data unavailable.  The application should at this point
+     * remove any references it has to the Loader's data.
+     *
+     * <p>This will always be called from the process's main thread.
+     *
+     * @param loader The Loader that is being reset.
+     */
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
     }
 }
